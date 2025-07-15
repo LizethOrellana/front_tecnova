e678246fd5c103190d932fce104e5945520229df
LizethOrellana <97999828+LizethOrellana@users.noreply.github.com>
2025-07-15 17:03:06 -0500

    cambios finales

--- src/paginas/historial-carrito/historial-carrito.component.ts
+++ src/paginas/historial-carrito/historial-carrito.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PedidoService } from '../../services/pedido.service';
 import { Pedido } from '../../models/Pedido';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-historial-carrito',
@@ -12,20 +13,24 @@ import { Pedido } from '../../models/Pedido';
 })
 export class HistorialCarritoComponent implements OnInit {
   historial: Pedido[] = [];
+  categoriasPorPagina = 10;
+  paginaActual = 1;
 
-  constructor(private pedidoService: PedidoService) { }
+  constructor(private pedidoService: PedidoService, private authService: AuthService) { }
 
   ngOnInit(): void {
-    const usuarioId = 1; // Idealmente: this.authService.getUserId()
-    this.pedidoService.obtenerPedidosPorUsuario(usuarioId).subscribe({
-      next: (data: Pedido[]) => {
-        this.historial = data;
-        console.log('Pedidos cargados', data);
-      },
-      error: (err: any) => {
-        console.error('Error al cargar historial', err);
-      }
-    });
+    const usuarioId = this.authService.getUserId()
+    if (usuarioId) {
+      this.pedidoService.obtenerPedidosPorUsuario(usuarioId).subscribe({
+        next: (data: Pedido[]) => {
+          this.historial = data;
+          console.log('Pedidos cargados', data);
+        },
+        error: (err: any) => {
+          console.error('Error al cargar historial', err);
+        }
+      });
+    }
   }
 
   eliminarPedido(id: number | undefined) {
@@ -42,6 +47,21 @@ export class HistorialCarritoComponent implements OnInit {
           alert('❌ No se pudo eliminar el pedido');
         }
       });
+    }
+  }
+
+  get categoriasPaginados() {
+    const inicio = (this.paginaActual - 1) * this.categoriasPorPagina;
+    return this.historial.slice(inicio, inicio + this.categoriasPorPagina);
+  }
+
+  get totalPaginas() {
+    return Math.ceil(this.historial.length / this.categoriasPorPagina);
+  }
+
+  cambiarPagina(nuevaPagina: number) {
+    if (nuevaPagina >= 1 && nuevaPagina <= this.totalPaginas) {
+      this.paginaActual = nuevaPagina;
     }
   }
 }
